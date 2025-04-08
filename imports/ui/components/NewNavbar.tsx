5578762de0164a9967c500a2150d0575afc18868
JungWoo Song <jsmac7014@gmail.com>
2025-04-08 13:38:47 +0900

    style: change to rounded-lg

--- imports/ui/components/NewNavbar.tsx
+++ imports/ui/components/NewNavbar.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import {useUser} from "meteor/react-meteor-accounts";
 import {Link} from "react-router-dom";
-import { Meteor } from "meteor/meteor";
+import {Meteor} from "meteor/meteor";
 
 export default function NewNavbar() {
     const user = useUser();
-
-    // @ts-ignore
-    const name = user ? user.profile?.name : "";
-    const username = user ? user.username : "";
 
     return (
         <div className="flex justify-between items-center w-full max-w-5xl mx-auto ">
@@ -20,10 +16,15 @@ export default function NewNavbar() {
             <div className="flex items-center">
                 {user ? (
                     <div className="inline-flex items-center gap-2">
-                        <Link to="/profile" className="underline font-semibold text-gray-700">
-                            {name || username}
+                        <Link to="/profile" className="bg-white rounded-lg border p-2 font-semibold text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
+                            </svg>
                         </Link>
-                        <button className="p-2 bg-white border rounded hover:bg-gray-50" onClick={() => Meteor.logout()}>
+                        <button className="p-2 bg-white border rounded-lg hover:bg-gray-50" onClick={() => Meteor.logout()}
+                                title="logout">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                                  stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round"
