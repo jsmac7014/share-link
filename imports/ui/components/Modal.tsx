7efec00598c859efa81c14406bb87e3f48970982
JungWoo Song <jsmac7014@gmail.com>
2025-04-10 00:16:09 +0900

    style: update modal transition

--- imports/ui/components/Modal.tsx
+++ imports/ui/components/Modal.tsx
@@ -15,18 +15,18 @@ const Modal: React.FC<ModalProps> = ({ children, onClose, isOpen, title }) => {
       {isOpen && (
         <motion.div
           key={"modal-backdrop"}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+          initial={{ opacity: 0, backdropFilter: "blur(0px)" }}
+          animate={{ opacity: 1, backdropFilter: "blur(2px)" }}
+          exit={{ opacity: 0, backdropFilter: "blur(0px)" }}
           className="fixed top-0 right-0 left-0 z-50 w-full h-dvh max-h-full bg-zinc-900 bg-opacity-30 flex justify-center items-center"
         >
           <motion.div
             key={"modal-content"}
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            exit={{ scale: 0 }}
-            transition={{ duration: 0.2 }}
-            className="relative bg-white rounded-lg w-full max-w-3xl p-2 m-8"
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0, opacity: 0 }}
+            transition={{ type: "spring", damping: 30, stiffness: 400 }}
+            className="relative bg-white rounded-lg w-full max-w-4xl p-2 m-4"
           >
             {/* Modal header*/}
             <div className="p-2 w-full inline-flex items-center justify-between">
