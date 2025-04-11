55441c845fe3e50bc878df57149de09f46ce1c7d
JungWoo Song <jsmac7014@gmail.com>
2025-04-11 21:13:26 +0900

    style: update bottom modal sheet height and max height

--- imports/ui/components/BottomSheetModal.tsx
+++ imports/ui/components/BottomSheetModal.tsx
@@ -35,7 +35,7 @@ export default function BottomSheetModal({
             animate={{ y: 0 }}
             exit={{ y: "100%" }}
             transition={{ duration: 0.3, type: "tween", ease: [0.65, 0.05, 0.36, 1] }}
-            className="fixed bottom-0 left-0 right-0 bg-white rounded-t-2xl shadow-lg p-4 z-99"
+            className="fixed bottom-0 left-0 right-0 bg-white rounded-t-2xl shadow-lg p-4 z-99 h-fit max-h-dvh overflow-y-auto"
           >
             {/* header */}
             <div className="flex flex-row justify-between items-center mb-2">
