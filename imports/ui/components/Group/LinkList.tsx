70db185d15db65a17a18f24ccb4c2512b40840c9
JungWoo Song <jsmac7014@gmail.com>
2025-04-11 21:13:55 +0900

    feat: updated LinkList to have bottom sheet modal

--- imports/ui/components/Group/LinkList.tsx
+++ imports/ui/components/Group/LinkList.tsx
@@ -1,12 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Meteor } from "meteor/meteor";
 import { useFind, useSubscribe } from "meteor/react-meteor-data";
 import { LinksWithUserInfo } from "/imports/api/client/linksWithUserInfo";
 import { toast } from "react-toastify";
 import { motion } from "motion/react";
 import { GroupFilterContext } from "/imports/ui/context/GroupFilterProvider";
+import { LinkWithUserInfo } from "/imports/types/types";
+import LinkBottomSheetModal from "/imports/ui/components/Group/LinkBottomSheetModal";
 
 export default function LinkList({ groupId, date }: { groupId: string; date: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedLink, setSelectedLink] = useState<LinkWithUserInfo | null>(null);
   // Get Timezone of the user
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   try {
@@ -64,68 +68,93 @@ export default function LinkList({ groupId, date }: { groupId: string; date: str
   }
 
   return (
-    <motion.ul
-      className="flex flex-col w-full gap-1"
-      layout
-      initial={{ opacity: 0, scale: 0.95 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.8, transition: { duration: 1 } }}
-    >
-      {links.map((link) => (
-        <motion.li
-          key={link._id?.toString()}
-          className="w-full"
-          layoutId={link._id}
-          initial={{ opacity: 0, scale: 0.95 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.2 } }}
-          transition={{ type: "tween", duration: 0.2 }}
-        >
-          <a href={link.url} target="_blank" rel="noopener noreferrer" className="w-full">
-            <div className="flex bg-white w-full h-full gap-2 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition duration-200 ease-in-out">
-              {link.imageLink && (
-                <img
-                  src={link.imageLink}
-                  className="md:w-32 md:h-32 w-16 aspect-square rounded-tl-lg rounded-bl-lg object-cover"
-                  alt={link.title}
-                />
-              )}
-              <div className="w-full flex flex-col overflow-hidden p-2">
-                <div className="flex flex-row gap-2">
-                  <p className="text-gray-500 text-xs">@{link.userInfo?.username}</p>
+    <div>
+      <motion.ul
+        className="flex flex-col w-full gap-1"
+        layout
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8, transition: { duration: 1 } }}
+      >
+        {links.map((link) => (
+          <motion.li
+            key={link._id?.toString()}
+            className="w-full"
+            layoutId={link._id}
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.2 } }}
+            transition={{ type: "tween", duration: 0.2 }}
+            onClick={() => {
+              setSelectedLink(link);
+              setIsOpen(true);
+            }}
+          >
+            <div className="w-full cursor-pointer">
+              <div className="flex bg-white w-full h-full gap-2 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition duration-200 ease-in-out">
+                {link.imageLink && (
+                  <img
+                    src={link.imageLink}
+                    className="md:w-32 md:h-32 w-16 aspect-square rounded-tl-lg rounded-bl-lg object-cover"
+                    alt={link.title}
+                  />
+                )}
+                <div className="w-full flex flex-col overflow-hidden p-2">
+                  <div className="flex flex-row gap-2">
+                    <p className="text-gray-500 text-xs">@{link.userInfo?.username}</p>
+                  </div>
+                  <div className="h-full flex flex-col justify-between">
+                    <div className="flex flex-col">
+                      <a
+                        className="text-gray-500 text-xs hover:text-blue-500"
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {link.url.length > 40 ? link.url.slice(0, 40) + "..." : link.url}
+                      </a>
+                      <h3 className="md:text-md text-sm font-bold line-clamp-2">{link.title}</h3>
+                      <p className="truncate text-sm">{link.description}</p>
+                    </div>
+                    <p className="text-gray-500 text-xs">
+                      {new Date(link.createdAt).toLocaleTimeString()}
+                    </p>
+                  </div>
                 </div>
-                <div className="flex flex-col">
-                  <h3 className="md:text-md text-sm font-bold line-clamp-2">{link.title}</h3>
-                  <p className="truncate">{link.description}</p>
-                  <p className="text-gray-500 text-xs">
-                    {new Date(link.createdAt).toLocaleTimeString()}
-                  </p>
-                </div>
-              </div>
-              <button
-                className="p-2 place-self-start hover:bg-gray-100"
-                onClick={(event) => {
-                  // prevent click event from anchor tag
-                  event.stopPropagation();
-                  event.preventDefault();
-                  deleteLink(link._id);
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-5 h-5 text-red-500"
+                <button
+                  className="p-2 place-self-start hover:bg-gray-100"
+                  onClick={(event) => {
+                    // prevent click event from anchor tag
+                    event.stopPropagation();
+                    event.preventDefault();
+                    deleteLink(link._id);
+                  }}
                 >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5 text-red-500"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex flex-col"></div>
             </div>
-          </a>
-        </motion.li>
-      ))}
-    </motion.ul>
+          </motion.li>
+        ))}
+      </motion.ul>
+      {selectedLink && (
+        <LinkBottomSheetModal
+          selectedLink={selectedLink}
+          groupId={groupId}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+      )}
+    </div>
   );
 }
